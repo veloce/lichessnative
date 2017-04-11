8d4d4e5e10e832afa29be51d2283f6d37a0b859f
Vincent Velociter <vincent.velociter@gmail.com>
2017-04-11 20:02:19 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component<void, void> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    )
+  }
+}
+
+interface Style {
+  container: React.ViewStyle,
+  welcome: React.TextStyle,
+  instructions: React.TextStyle
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
